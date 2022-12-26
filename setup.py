5ab7268639d94e0b5f0daebf68083a303d3ef2de
Robert <robertvandevlasakker@outlook.com>
2022-12-26 11:45:35 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Internship documents, code, figures etc. for the (remote) internship at INRIA.',
+    author='Robert van de Vlasakker',
+    license='MIT',
+)
